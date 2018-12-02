13bc1b0a23f5aa3ba9c31e7630c0b32f6834f4c5
Peter <peter@initiatenorth.co.uk>
2018-12-02 20:55:53 +0000

    Day02 part 1

---
+++ AdventOfCode2018/Day02.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode2018
+{
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Linq;
+
+    public class Day02
+    {
+        private const int Zero = 0;
+        private const int One = 1;
+        private const int Two = 2;
+        private const int Three = 3;
+
+        public static ConcurrentDictionary<char, int> GetCharAndSum(string chars)
+        {
+            var charsAndCount = new ConcurrentDictionary<char, int>();
+
+            chars.ToList().ForEach(c => { charsAndCount.AddOrUpdate(c, One, (id, count) => count + One); });
+
+            return charsAndCount;
+        }
+
+        public static int GetIncrementAmount(bool value) => value ? One : Zero;
+
+        public static int GetChecksum(string path)
+        {
+            var checksum = File.ReadAllLines(path)
+                .Aggregate(new { Twos = Zero, Threes = Zero },
+                    (twosAndThrees, row) =>
+                    {
+                        var charsAndCount = GetCharAndSum(row);
+                        
+                        return new
+                        {
+                            Twos = twosAndThrees.Twos + GetIncrementAmount(charsAndCount.Any(kvp => kvp.Value == Two)),
+                            Threes = twosAndThrees.Threes + GetIncrementAmount(charsAndCount.Any(kvp => kvp.Value == Three))
+                        };
+                    });
+
+            return checksum.Twos * checksum.Threes;
+        }
+    }
+}
---
+++ AdventOfCode2018Tests/Day02Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2018Tests
+{
+    using AdventOfCode2018;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Day02Tests
+    {
+        private readonly string path = @"C:\src\AdventOfCode\AdventOfCode2018Tests\resources\Day02.input";
+
+        [TestMethod]
+        public void ShouldGetCorrectChecksum()
+        {
+            var result = Day02.GetChecksum(path);
+            Assert.AreEqual(5681, result);
+        }
+    }
+}
