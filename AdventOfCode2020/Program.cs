cfefc3731ac2b37406661e55f1282f5b87ce3f71
peteski <peteski22@users.noreply.github.com>
2020-12-01 11:29:59 +0000

    tweakers for console output

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -9,6 +9,9 @@
             Console.WriteLine("DAY 1");
             Console.WriteLine("Part 1:" + Day01.GetPart1Result());
             Console.WriteLine("Part 2:" + Day01.GetPart2Result());
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     } 
 }
