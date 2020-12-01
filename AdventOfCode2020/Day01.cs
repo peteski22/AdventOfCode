4520043fd155e01de6cb4a6f39576c39dae68b66
peteski <peteski22@users.noreply.github.com>
2020-12-01 11:29:06 +0000

    shifted day 1 out of the way

---
+++ AdventOfCode2020/Day01.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2020
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class Day01
+    {
+
+        private static readonly string inputPath = @"C:\src\AdventOfCode\AdventOfCode2020\input\day01.input";
+
+        private static readonly List<int> input = File.ReadAllLines(inputPath).Select(line => int.Parse(line)).ToList();
+
+        public static int GetPart1Result()
+        {
+            return (from int i in input
+                    from int j in input
+                    where i + j == 2020
+                    select i * j).Distinct().First();
+        }
+
+        public static int GetPart2Result()
+        {
+            return (from int i in input
+                    from int j in input
+                    from int k in input
+                    where i + j + k == 2020
+                    select i * j * k).Distinct().First();
+        }
+
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,27 +1,14 @@
 ﻿namespace AdventOfCode2020
 {
     using System;
-    using System.IO;
-    using System.Linq;
 
     public class Program
     {
         static void Main(string[] args)
         {
-            var inputPath = @"C:\src\AdventOfCode\AdventOfCode2020\input\day01.input";
-
-            var list = File.ReadAllLines(inputPath).Select(line => int.Parse(line)).ToList();
-
-            var result = (from int i in list
-                          from int j in list
-                          where i + j == 2020
-                          select i * j).Distinct().First();
-
-            var result2 = (from int i in list
-                           from int j in list
-                           from int k in list
-                           where i + j + k == 2020
-                           select i * j * k).Distinct().First();
+            Console.WriteLine("DAY 1");
+            Console.WriteLine("Part 1:" + Day01.GetPart1Result());
+            Console.WriteLine("Part 2:" + Day01.GetPart2Result());
         }
     } 
 }
